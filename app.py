a3aa4ac4b52b202ba6e475a8dbbaabbad4062e91
AkashHiremath856 <akash.hiremath25@gmail.com>
2023-02-23 17:20:32 +0530

    Validation Added

--- app.py
+++ app.py
@@ -15,48 +15,62 @@ st.text('(Model is limited to classes given by the user)')
 # User Input
 st.text('Write The Class (eg, Train, Car, etc) ')
 
-User_input = [st.text_input('Class 1'), st.text_input(
-    'Class 2'), st.text_input('Class 3')]  # arg1
+select_box = st.selectbox('Select Total Classes', [2, 3, 4, 5])
+
+User_input = [st.text_input(f'Class {_}')
+              for _ in range(1, select_box+1)]  # arg1
+
 n = st.text_input('Enter Number Of Images (>10 recommended)')  # arg2
 
-print(User_input)
-if User_input == ['', '', '']:
-    st.warning('Please Input the classes')
+if '' in User_input:
+    st.warning('Input required')
 else:
+    try:
+        n = int(n)
+    except:
+        st.warning('Enter a valid number')
+
     if st.button('Train Model'):
-        st.write('Training Model...')
-        get_images(User_input, int(n))  # func get_images
-        train_model_()  # function tain_model
+        if n < 10:
+            st.warning('Number greater than 10 expected')
+            try:
+                folder = (os.listdir('Images')[0])
+                if os.listdir('Images') != User_input or (len(os.listdir(f'Images/{folder}'))):
+                    get_images(User_input, int(n))  # func get_images
+            except:
+                get_images(User_input, int(n))
+            st.write('Training Model...')
+            train_model_()  # function tain_model
 
-# Prediction
+            # Prediction
 
-st.text('Upload the Image')
+            st.text('Upload the Image')
 
-model = pickle.load(open('img_model.p', 'rb'))
+            model = pickle.load(open('img_model.p', 'rb'))
 
-uploaded_file = st.file_uploader('Choose an image', type='jpg')
+            uploaded_file = st.file_uploader('Choose an image', type='jpg')
 
-test_url = st.text_input('Enter the test image url')
+            test_url = st.text_input('Enter the test image url')
 
-# results of uploads
+            # results of uploads
 
-if uploaded_file is not None or test_url != '':
-    if test_url != "":
-        img = imread(test_url)
-    else:
-        img = Image.open(uploaded_file)
-    st.image(img, 'Uploaded Image')
+            if uploaded_file is not None or test_url != '':
+                if test_url != "":
+                    img = imread(test_url)
+                else:
+                    img = Image.open(uploaded_file)
+                st.image(img, 'Uploaded Image')
 
-    if st.button('PREDICT CLASS'):
-        flatten_data = []
-        DATA_DIR = 'Images/'
-        CATEGORIES = os.listdir(DATA_DIR)
-        st.write('Results...')
+                if st.button('PREDICT CLASS'):
+                    flatten_data = []
+                    DATA_DIR = 'Images/'
+                    CATEGORIES = os.listdir(DATA_DIR)
+                    st.write('Results...')
 
-        img = np.array(img)
-        img_resized = resize(img, (150, 150, 3))
-        flatten_data.append(img_resized.flatten())
-        flatten_data = np.array(flatten_data)
-        y_out = model.predict(flatten_data)
-        y_out = CATEGORIES[y_out[0]]
-        st.write(f' Predicted Class is {y_out.upper()}')
+                    img = np.array(img)
+                    img_resized = resize(img, (150, 150, 3))
+                    flatten_data.append(img_resized.flatten())
+                    flatten_data = np.array(flatten_data)
+                    y_out = model.predict(flatten_data)
+                    y_out = CATEGORIES[y_out[0]]
+                    st.write(f' Predicted Class is {y_out.upper()}')
